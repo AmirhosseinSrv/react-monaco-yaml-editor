e1b8b929d8cd21eeb8ed7e604f9d74a22a378006
Amirhossein Sarvari <amirhossein.srv@gmail.com>
2022-12-16 19:27:27 +0330

    React Monaco YAML Editor Implementation

---
+++ src/types.d.ts
@@ -0,0 +1,24 @@
+declare module 'monaco-editor/esm/vs/editor/common/services/languageFeatures.js' {
+  export const ILanguageFeaturesService: { documentSymbolProvider: unknown };
+}
+  
+declare module 'monaco-editor/esm/vs/editor/contrib/documentSymbols/browser/outlineModel.js' {
+  import { editor, languages } from 'monaco-editor';
+
+  export abstract class OutlineModel {
+    static create(registry: unknown, model: editor.ITextModel): Promise<OutlineModel>;
+
+    asListOfDocumentSymbols(): languages.DocumentSymbol[];
+  }
+}
+  
+declare module 'monaco-editor/esm/vs/editor/standalone/browser/standaloneServices.js' {
+  export const StandaloneServices: {
+    get: (id: unknown) => { documentSymbolProvider: unknown };
+  };
+}
+  
+declare module '*.json' {
+  declare const uri: string;
+  export default uri;
+}
